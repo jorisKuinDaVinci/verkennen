1d6634cd5145dc74e0c1ac662a6c6bdd4a32fe52
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-01-16 09:40:42 +0100

    les 3 van module 3 begonnen

---
+++ module_3/deel_1/les 3/GUI/feestlunch0302.js
@@ -0,0 +1,15 @@
+//kosten van de feestlunch
+CROISSANTJES = 0.39
+STOKBRODEN = 2.78
+KORTINGSBONNEN = 0.50
+//aantal
+AANTAL_CROISSANTJES = 17
+AANTAL_STOKBRODEN = 2
+AANTAL_KORTINGSBONNEN = 3
+//prijs
+prijs_croissantjes = AANTAL_CROISSANTJES * CROISSANTJES
+prijs_stokbroden = AANTAL_STOKBRODEN * STOKBRODEN
+prijs_kortingsbonnen = AANTAL_KORTINGSBONNEN * KORTINGSBONNEN
+//totaal
+totaal = prijs_croissantjes + prijs_stokbroden - prijs_kortingsbonnen
+print(f"De feestlunch kost je bij de bakker: {totaal} euro voor de {AANTAL_CROISSANTJES} croissantjes en de {AANTAL_STOKBRODEN} stokbroden als de {AANTAL_KORTINGSBONNEN} kortingsbonnen nog geldig zijn!")
