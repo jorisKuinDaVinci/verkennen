7963158165ef5c0edc9acad3c1ef2f4bd8b5e4ba
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-01-22 14:55:30 +0100

    wedstrijdteller voor eerste les klaar, verder in 2de les

--- module_3/deel_2/wedstrijdteller/wedstrijdteller.js
+++ module_3/deel_2/wedstrijdteller/wedstrijdteller.js
@@ -1,9 +1,51 @@
 inputTeams = document.getElementById('input-teams');
 
+game =
+{
+    team1Punten: 0,
+    team2Punten: 1,
+    serving:0   
+} 
+
 inputTeam1.value = '...'; // testdata
 inputTeam2.value = '...'; // testdata
 
+function updateScreen(){
+    // maak juiste button geel!
+    if (game.serving == 1) {
+        counterTeam1.classList.add('serving');
+        counterTeam2.classList.remove('serving');
+    } else {
+        counterTeam2.classList.add('serving');
+        counterTeam1.classList.remove('serving');
+    }
+}    
+
 function start(event){
+    console.log('You pressed start!');
+    if (nameTeam1.innerText == '...' || nameTeam2.innerText == '...') {
+        alert('Er mist een naam!');
+        return;
+    };
+    //
+    console.dir(servingTeam1);
+    if (!servingTeam1.checked && !servingTeam2.checked) {
+        alert('Wie moet er beginnen?');
+        return;
+    };
+
+    // hide inputfields
+    input_teams.style.display = 'none';
+    // activate serving
+    counterTeam1.classList.add('serving');
+
+    game.serving = 2;
+    if (servingTeam2.checked) {
+        game.serving = 1;
+    };
+
+    updateScreen();
+
 }
 startButton.addEventListener('click',start);
 
@@ -13,7 +55,8 @@ counterTeam1.addEventListener('click',count);
 counterTeam2.addEventListener('click',count);
 
 function displayNames(event){
-    nameTeam1.innertext = inputTeam1.value;
+    nameTeam1.innerText = inputTeam1.value;
+    nameTeam2.innerText = inputTeam2.value;
 
 }
 inputTeam1.addEventListener('change',displayNames);
