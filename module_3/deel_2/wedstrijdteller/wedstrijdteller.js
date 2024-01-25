95b488886e33b7106f928e64a137ac1c0fa31e4d
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-01-25 11:44:19 +0100

    wedstrijdteller bijgewerkt

--- module_3/deel_2/wedstrijdteller/wedstrijdteller.js
+++ module_3/deel_2/wedstrijdteller/wedstrijdteller.js
@@ -1,10 +1,12 @@
 inputTeams = document.getElementById('input-teams');
 
-game =
+let game =
 {
     team1Punten: 0,
     team2Punten: 1,
-    serving:0   
+    serving:0,
+    lastServe:0, // wie vorige service
+    lastScored:0  
 } 
 
 inputTeam1.value = '...'; // testdata
@@ -19,6 +21,9 @@ function updateScreen(){
         counterTeam2.classList.add('serving');
         counterTeam1.classList.remove('serving');
     }
+
+    counterTeam1.innerText = game.team1Punten;
+    counterTeam2.innerText = game.team2Punten;
 }    
 
 function start(event){
@@ -36,11 +41,12 @@ function start(event){
 
     // hide inputfields
     input_teams.style.display = 'none';
-    // activate serving
-    counterTeam1.classList.add('serving');
+    // Activeer tellers
+    counterTeam1.disabled = false;
+    counterTeam2.disabled = false;
 
     game.serving = 2;
-    if (servingTeam2.checked) {
+    if (servingTeam1.checked) {
         game.serving = 1;
     };
 
@@ -50,6 +56,19 @@ function start(event){
 startButton.addEventListener('click',start);
 
 function count(event){
+    game.lastServe = game.serving;
+    if (this.id == 'counterTeam1') {
+        game.team1Punten +=1;
+        game.serving = 1;
+        game.lastScored = 1;
+    } else {
+        game.team2Punten +=1;
+        game.serving = 2;
+        game.lastScored = 2;
+    }
+
+    undoButton.disabled = false;
+    updateScreen();
 }
 counterTeam1.addEventListener('click',count);
 counterTeam2.addEventListener('click',count);
@@ -61,3 +80,29 @@ function displayNames(event){
 }
 inputTeam1.addEventListener('change',displayNames);
 inputTeam2.addEventListener('change',displayNames);
+
+function UndoLastPoint(event){
+    undoButton.disabled = true;
+    if (game.lastScored == 1) {
+        game.team1Punten -=1;
+        game.serving = 2;
+    } else {
+        game.team2Punten -=1;
+        game.serving = 1;
+    }
+    updateScreen();
+}
+undoButton.addEventListener('click',UndoLastPoint);{
+    undoButton.disabled = true;
+    if (game.lastScored == 1) {
+        game.team1Punten -=1;
+        game.serving = 2;
+    } else {
+        game.team2Punten -=1;
+        game.serving = 1;
+    }
+}
+
+undoButton.disabled = true;
+counterTeam1.disabled = true;
+counterTeam2.disabled = true;
