076ffc1b5f8de16e4ccd4a00e733309f125038cc
jorisKuinDaVinci <99074486@mydavinci.nl>
2024-02-16 10:31:32 +0100

    button opdracht verder gewerkt

--- module_3/deel_2/test/test.js
+++ module_3/deel_2/test/test.js
@@ -1,12 +1,20 @@
-
+let teller = 0;
 function buttonStart() {
     console.log("U hebt op de knop geklikt!");
     if (mijnBody.classList.contains('black')){
         mijnBody.classList.toggle('yellow');
     }
-    //pakButton.classList.add('yellow');
-    //pakButton.classList.remove('black');
-    //console.log("De kleur van de knop is nu: " + pakButton.classList);
+    teller++;
+    console.log("U hebt " + teller + " keer op de knop geklikt!");
+    counter.innerText = "aantal clicks: " + teller;
     
 }
+function buttonmove() {
+    pakButton.style.position ='absolute';
+    pakButton.style.top = Math.floor(Math.random()*90+5)+'%';
+    pakButton.style.left = Math.floor(Math.random()*90+5)+'%';
+    pakButton.innerText = "Pak me!";
+}
+setInterval (buttonmove, 1000);
+//sets a random absolute position to a html element; receives the html element
 pakButton.addEventListener("click", buttonStart);
